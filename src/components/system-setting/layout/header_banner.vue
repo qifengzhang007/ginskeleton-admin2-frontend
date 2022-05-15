121be8f7a3c1434a5f299a84bcf4767d14d7c886
张奇峰 <1990850157@qq.com>
2022-05-16 00:26:04 +0800

    保存: 开始开发右上角用户图标相关功能

--- src/components/system-setting/layout/header_banner.vue
+++ src/components/system-setting/layout/header_banner.vue
@@ -4,23 +4,42 @@
       <span class="collapse">       <el-icon><Fold/></el-icon>    </span>
       <span class="menu-nav">
         <el-breadcrumb separator-icon="ArrowRight">
-    <el-breadcrumb-item :to="{ path: '/' }">  <el-icon :size="16" style="vertical-align: middle">    <HomeFilled />    </el-icon> <span style="vertical-align: middle;color: #858585">首页</span></el-breadcrumb-item>
+    <el-breadcrumb-item :to="{ path: '/' }">  <el-icon :size="16" style="vertical-align: middle">    <HomeFilled/>    </el-icon> <span style="vertical-align: middle;color: #858585">首页</span></el-breadcrumb-item>
     <el-breadcrumb-item v-for="item  in menuStore.getMenuNavPathList(tabsStore.tabs.curMenuItem.relaMenuId)">{{ item.title }}</el-breadcrumb-item>
   </el-breadcrumb>
       </span>
     </div>
 
-    <div class="header-banner-right">管理员</div>
+    <div class="header-banner-right">
+      <el-icon :size="24">
+        <FullScreen/>
+      </el-icon>
+      <el-avatar shape="circle" :size="50" src="http://139.196.101.31:20201/storage/uploaded/2021_03/0cab2657f435a6424d5716ec8ce6c847.png"/>
+
+      <el-dropdown size="small" @command="handleCommand">
+    <span class="el-dropdown-link">
+      <el-icon class="el-icon--right">
+        <arrow-down/>
+      </el-icon>
+    </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item command="loginOut">退出登陆</el-dropdown-item>
+            <el-dropdown-item divided command="edit">编辑信息</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+
+    </div>
 
   </div>
 
 </template>
 
 <script>
-import {useHeaderBannerStore} from '@/store/system-setting/header_banner'
 import {useMenuStore} from '@/store/system-setting/menu'
 import {useTabStore} from '@/store/system-setting/tabs'
-import {reactive, ref, toRefs} from 'vue'
+import {useRouteStore} from '@/store/system-setting/route'
 
 
 export default {
@@ -33,15 +52,30 @@ export default {
     // })
 
     const menuStore = useMenuStore()
-    const headerBannerStore = useHeaderBannerStore()
     const tabsStore = useTabStore()
+    const routerStore = useRouteStore()
+    // const headerBannerStore = useHeaderBannerStore()
+
+
+    const handleCommand = (command) => {
+      switch (command) {
+        case 'loginOut':
+          console.log('退出登陆')
+          routerStore.getRoute.push('/login')
+          break
+        case 'edit':
+          console.log('编辑个人信息')
+          break
+      }
+    }
+
 
     return {
       // ...toRefs(stateData),
       // headerBannerStore,
       tabsStore,
       menuStore,
-
+      handleCommand
     }
 
   }
@@ -82,11 +116,14 @@ export default {
 }
 
 .header-banner-right {
-  display: inline-block;
+  display: flex;
   float: right;
   width: 18%;
-  text-align: right;
-  padding-right: 5px;
+  height: 100%;
+  padding-right: 15px;
+  vertical-align: middle;
+  align-items: center;
+  justify-content: flex-end;
 }
 
 </style>
