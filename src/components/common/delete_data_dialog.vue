cbb54b35aff72a6fe113413a50fff64072a718c1
张奇峰 <1990850157@qq.com>
2022-05-26 22:55:30 +0800

    单词拼写错误修正

--- src/components/common/delete_data_dialog.vue
+++ src/components/common/delete_data_dialog.vue
@@ -8,20 +8,20 @@
         {{ propDelete.actionName }}
       </div>
     </template>
-    <span style="{font-size: 13px}">	 本次将删除 <span class="hight-light">{{ propDelete.delCounts }} </span> 条数据, 确认删除吗 ？</span>
+    <span style="{font-size: 13px}">	 本次将删除 <span class="high-light">{{ propDelete.delCounts }} </span> 条数据, 确认删除吗 ？</span>
     <br/> <br/>
     <template v-if="propDelete.serverResCode===200">
       删除结果：
-      <span class="hight-light">成功</span>
+      <span class="high-light">成功</span>
     </template>
     <template v-else-if="propDelete.serverResCode===400">
-      删除结果：<span class="hight-light">失败 , {{ propDelete.serverResMsg }}</span>
+      删除结果：<span class="high-light">失败 , {{ propDelete.serverResMsg }}</span>
     </template>
     <template v-else-if="propDelete.serverResCode!==0">
       <el-icon color="#ef2f2f">
         <WarningFilled/>
       </el-icon>
-      删除操作出错：<span class="hight-light">{{ propDelete.serverResMsg }}</span>
+      删除操作出错：<span class="high-light">{{ propDelete.serverResMsg }}</span>
     </template>
     <template #footer>
       <span class="dialog-footer">
@@ -69,7 +69,7 @@ export default {
 </script>
 
 <style scoped>
-.hight-light {
+.high-light {
   color: #f14a4a;
 }
 </style>
