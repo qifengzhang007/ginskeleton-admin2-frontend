0ee581b4e6370f3f94bcee6d6cfbc2129af4e411
张奇峰 <1990966837@qq.com>
2022-06-09 11:58:49 +0800

    默认首页内容更新

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,8 +2,8 @@
   <div class="index_style">
     <h1> 主线版本: <a target="_blank" href="https://gitee.com/daitougege/GinSkeleton">GinSkeleton</a></h1>
     <span style="color:lightslategray"> Admin2 系统基于 GinSkeleton(v1.5.xx + javascript + elementPlus + Vite2.x +Vue3.x + vue-router4.x + pinia 等最新技术开发</span>
-    <h5>后端仓库地址： <a target="_blank" href="https://gitee.com/daitougege/gin-skeleton-admin2-backend">GinSkeleton-Admin-Backend</a></h5>
-    <h5>前端仓库地址： <a target="_blank" href="https://gitee.com/daitougege/gin-skeleton-admin2-frontend">GinSkeleton-Admin-Frontend</a></h5>
+    <h5>后端仓库地址： <a target="_blank" href="https://gitee.com/daitougege/gin-skeleton-admin2-backend">GinSkeleton-Admin2-Backend</a></h5>
+    <h5>前端仓库地址： <a target="_blank" href="https://gitee.com/daitougege/gin-skeleton-admin2-frontend">GinSkeleton-Admin2-Frontend</a></h5>
   </div>
 </template>
 
