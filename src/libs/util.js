635d5ad643f95e5fbbb3f7614b49ac31c2b8af72
张奇峰 <1990966837@qq.com>
2023-03-01 16:58:15 +0800

    删除无关注释

--- src/libs/util.js
+++ src/libs/util.js
@@ -45,8 +45,8 @@ export const clearCookie = () => {
     const keys = document.cookie.match(/[^ =;]+(?==)/g);
     if (keys) {
         for (let i = keys.length; i--;) {
-            document.cookie = keys[i] + '=0;path=/;expires=' + new Date(0).toUTCString();//清除当前域名下的
-            document.cookie = keys[i] + '=0;path=/;domain=' + document.domain + ';expires=' + new Date(0).toUTCString();//清除当前域名下的
+            document.cookie = keys[i] + '=0;path=/;expires=' + new Date(0).toUTCString();
+            document.cookie = keys[i] + '=0;path=/;domain=' + document.domain + ';expires=' + new Date(0).toUTCString();
         }
     }
 }
