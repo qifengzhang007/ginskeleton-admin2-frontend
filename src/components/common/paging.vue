a0cf255df22a4a2a59517d7a13958ddb49377713
张奇峰 <1990850157@qq.com>
2023-08-02 16:04:49 +0800

    修复bug 1.语言包设置中存在的一处报错.

--- src/components/common/paging.vue
+++ src/components/common/paging.vue
@@ -1,62 +1,60 @@
 <template>
-  <ElConfigProvider :locale="locale">
-    <el-pagination small="small" layout="total,prev, pager, next,sizes" :default-page-size="20" prev-text="上一页" next-text="下一页" background
-                   :total="propPage.total" v-model:current-page="propPage.searchKeyWords.page" v-model:page-size="propPage.searchKeyWords.limit" :page-sizes="pageOptions"
-    />
-  </ElConfigProvider>
+    <ElConfigProvider>
+        <el-pagination small="small" layout="total,prev, pager, next,sizes" :default-page-size="20" prev-text="上一页" next-text="下一页" background
+                       :total="propPage.total" v-model:current-page="propPage.searchKeyWords.page" v-model:page-size="propPage.searchKeyWords.limit" :page-sizes="pageOptions"
+        />
+    </ElConfigProvider>
 </template>
 
 <script>
 import commonFunc from '@/libs/common_func'
 import {reactive, toRefs, watch} from "vue";
 import {ElConfigProvider} from 'element-plus'
-import chCn from 'element-plus/lib/locale/lang/zh-cn'
 
 export default {
-  name: "Paging",
-  components: {
-    ElConfigProvider
-  },
-  props: {
-    propPage: Object,
-  },
-  emits: ['fPageCallback'],
-  setup(props, context) {
-    const {propPage} = toRefs(props)
-    const stateData = reactive({
-      pageOptions: commonFunc.PageSizeOptions,
-      tmpOldPage:1,
-      tmpOldLimit:20,
-    })
-    stateData.tmpOldPage=propPage.value.searchKeyWords.page
-    stateData.tmpOldLimit=propPage.value.searchKeyWords.limit
-    // 分页组件改变事件,通过监听绑定的变量实现，
-    // elementPlus 官方提示，不要使用相关事件实现，后续版本会移出事件模式
-    watch(() => propPage.value.searchKeyWords.page, (newPage, oldPage) => {
-      if(stateData.tmpOldPage!==newPage){
-        context.emit('fPageCallback')
-      }
-      stateData.tmpOldPage=newPage
-    }, {immediate: true})
+    name: "Paging",
+    components: {
+        ElConfigProvider
+    },
+    props: {
+        propPage: Object,
+    },
+    emits: ['fPageCallback'],
+    setup(props, context) {
+        const {propPage} = toRefs(props)
+        const stateData = reactive({
+            pageOptions: commonFunc.PageSizeOptions,
+            tmpOldPage: 1,
+            tmpOldLimit: 20,
+        })
+        stateData.tmpOldPage = propPage.value.searchKeyWords.page
+        stateData.tmpOldLimit = propPage.value.searchKeyWords.limit
+        // 分页组件改变事件,通过监听绑定的变量实现，
+        // elementPlus 官方提示，不要使用相关事件实现，后续版本会移出事件模式
+        watch(() => propPage.value.searchKeyWords.page, (newPage, oldPage) => {
+            if (stateData.tmpOldPage !== newPage) {
+                context.emit('fPageCallback')
+            }
+            stateData.tmpOldPage = newPage
+        }, {immediate: true})
 
-    watch(() => propPage.value.searchKeyWords.limit, (newLimit, oldLimit) => {
-      if(stateData.tmpOldLimit!==newLimit){
-        context.emit('fPageCallback')
-      }
-      stateData.tmpOldLimit=newLimit
-    }, {immediate: true})
+        watch(() => propPage.value.searchKeyWords.limit, (newLimit, oldLimit) => {
+            if (stateData.tmpOldLimit !== newLimit) {
+                context.emit('fPageCallback')
+            }
+            stateData.tmpOldLimit = newLimit
+        }, {immediate: true})
 
-    return {
-      ...toRefs(stateData),
-      propPage,
-      locale: chCn
+        return {
+            ...toRefs(stateData),
+            propPage
+        }
     }
-  }
 }
 </script>
 
 <style>
 div.el-pagination {
-  display: inline-flex;
+    display: inline-flex;
 }
 </style>
